19021726ab1e604d25191b9d76e9d5de58126079
mac_zhu <zfm19980618@icloud.com>
2020-02-29 20:28:17 +0800

    finish iterative preoder

--- top_400/tree/144_binary_tree_preorder_traversal.py
+++ top_400/tree/144_binary_tree_preorder_traversal.py
@@ -5,6 +5,25 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+# # Recursive
+# class Solution(object):
+#     def preorderTraversal(self, root):
+#         """
+#         :type root: TreeNode
+#         :rtype: List[int]
+#         """
+#         ret = []
+#         if root == None:
+#             print("null")
+#             return ret
+#         else:
+#             print(root.val)
+#             ret.append(root.val)
+#             ret += self.preorderTraversal(root.left)
+#             ret += self.preorderTraversal(root.right)
+#             return ret
+
+# Iterative
 class Solution(object):
     def preorderTraversal(self, root):
         """
@@ -12,17 +31,21 @@ class Solution(object):
         :rtype: List[int]
         """
         ret = []
-        if root == None:
-            print("null")
-            return ret
-        else:
-            print(root.val)
-            ret.append(root.val)
-            ret += self.preorderTraversal(root.left)
-            ret += self.preorderTraversal(root.right)
-            return ret 
+        call_stack = [root]
+        while  call_stack != []:
+            # print(call_stack)
+            curr = call_stack.pop()
+            if curr == None:
+                pass
+            else:
+                # print(curr.val)
+                ret.append(curr.val)
+                call_stack.append(curr.right)
+                call_stack.append(curr.left)
+        return ret        
 
 sol = Solution()
 root = TreeNode(1)
 root.right = TreeNode(3)
 print(sol.preorderTraversal(root))
+
