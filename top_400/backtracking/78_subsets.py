e8d58564f45ae53d45263c5393fe180bbff90371
mac_zhu <zfm19980618@icloud.com>
2020-03-04 17:03:50 +0800

    iterative and backtracking subsets

--- top_400/backtracking/78_subsets.py
+++ top_400/backtracking/78_subsets.py
@@ -19,29 +19,66 @@ Output:
 ]
 '''
 
-class Solution(object):
+# # Recursive
+# class Solution(object):
+#     def subsets(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[List[int]]
+#         """
+#         if len(nums) == 0:
+#             return []
+#         if len(nums) == 1:
+#             # print("in")
+#             return [nums, []]
+#         # print(nums_no_last)
+#         ret = self.subsets(nums[:-1])
+#         # print(subset_no_last)
+#         for i in range(len(ret)):
+#             ret.append(ret[i] + [nums[-1]])
+#         # print(subset_no_last)
+#         return ret
+
+# # Iterative
+# class Solution(object):
+#     def subsets(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[List[int]]
+#         """
+#         ret = [[]]
+#         for i in range(len(nums)):
+#             for j in range(len(ret)):
+#                 ret.append(ret[j] + [nums[i]])
+#         return ret
+
+# Backracking
+class Solution:
     def subsets(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        if len(nums) == 0:
-            return []
-        if len(nums) == 1:
-            # print("in")
-            return [nums, []]
-        nums_no_last = nums[0:-1]
-        nums_last = nums[-1]
-        # print(nums_no_last)
-        subset_no_last = self.subsets(nums_no_last)
-        # print(subset_no_last)
-        for i in range(len(subset_no_last)):
-            subset_no_last.append(subset_no_last[i] + [nums_last])
-        # print(subset_no_last)
-        return subset_no_last
+        def backtrack(first = 0, curr = []):
+            # if the combination is done
+            if len(curr) == k:  
+                output.append(curr[:])
+            for i in range(first, n):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+                # use next integers to complete the combination
+                backtrack(i + 1, curr)
+                # backtrack
+                curr.pop()
+        
+        output = []
+        n = len(nums)
+        for k in range(n + 1):
+            backtrack()
+        return output
 
 
 sol = Solution()
-nums = [1]
+nums = [1, 2, 3]
 print(len(sol.subsets(nums)))
 print(sol.subsets(nums))
